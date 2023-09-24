2ece4c67773a6495ecbc149df91274bfca297efc
Marwan Samy <90709517+marwan995@users.noreply.github.com>
2023-09-24 16:02:16 +0300

    v1

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -29,17 +29,26 @@ const Computers = ({isMobile}) => {
 }
 const ComputersCanvas = ()=>{
   const [isMobile, setisMobile] = useState(false)
-  useEffect(()=>{
-    const mediaQ = window.matchMedia('(max-width:500px)')
-    setisMobile(mediaQ.matches)
-    const handlemediaQ=(event)=>{
-        setisMobile(event.matchMedia)
-    }
-    mediaQ.addEventListener('change',handlemediaQ)
-    return () => {
-      mediaQ.removeEventListener("change", setisMobile);
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setisMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setisMobile(event.matches);
     };
-  },[])
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop='demand'
